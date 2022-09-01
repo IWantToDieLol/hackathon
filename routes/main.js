30b36b929a9f72ab242cede8d3596c19b4275039
Jaden loh <65410149+jadenlohh@users.noreply.github.com>
2022-09-01 21:24:20 +0800

    add NETS api

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const crypto = require('crypto');
 const paypal = require('paypal-rest-sdk');
 const router = express.Router();
+
+router.use(express.urlencoded({ extended: true }));
 
 PAYPAL_CLIENT_ID = "AS4V9XrjamXnmMRtae6c_D61Gsj27LQC7OzughnPMqBieYuVcNSGuhGVIRc3aiHLkiy1fJDgwlgr5N55"
 PAYPAL_CLIENT_SECRET = "EENDGdxtB9nXX8x0uKuk8R9RYx5IBg0HTe19S7gVLbYtX-DrD0iJFvX1u339Dnqxnn1ahdU6i6VzJr9n"
@@ -29,5 +32,55 @@ router.post('/purchasedetails', async function (req, res){
 router.get('/successpage', (req, res) => {
 	res.render('successpage')
 });
+
+router.post('/payment', (req, res) => {
+    const { donationAmount, customAmount } = req.body;
+
+    const getDate = function(n) {
+        const d = new Date();
+
+        var getMonth = d.getMonth() < 10 ? '0' + parseInt(d.getMonth() + 1) : d.getMonth() + 1;
+        var getDay = d.getDay() < 10 ? '0' + d.getDay() : d.getDay();
+
+        var getMinute = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+        var getSeconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
+
+        if (n == 'merchantTxnRef') {
+            return d.getFullYear() + getMonth + getDay + ' ' + d.getHours() + ':' + getMinute + ':' + getSeconds + '.' + Math.floor(d.getMilliseconds() / 10);
+        }
+        else {
+            return d.getFullYear() + getMonth + getDay + ' ' + d.getHours() + ':' + getMinute + ':' + getSeconds + '.' + d.getMilliseconds();
+        }
+    }
+
+    var txnReq = {
+        "ss": "1", 
+        "msg": {
+            "netsMid": "UMID_877772003", 
+            "tid": "", 
+            "submissionMode": "B", 
+            "txnAmount": donationAmount != undefined ? donationAmount * 100 : customAmount * 100, 
+            "merchantTxnRef": getDate('merchantTxnRef'), 
+            "merchantTxnDtm": getDate(), 
+            "paymentType": "SALE", 
+            "currencyCode": "SGD", 
+            "paymentMode": 'QR', 
+            "merchantTimeZone": "+8:00", 
+            "b2sTxnEndURL": "http://localhost:5000/successpage", 
+            "clientType": "W", 
+            "supMsg": "", 
+            "netsMidIndicator": "U", 
+            "ipAddress": "127.0.0.1", 
+            "language": "en"
+        }
+    }
+
+    var keyID = '154eb31c-0f72-45bb-9249-84a1036fd1ca';
+    var secretKey = '38a4b473-0295-439d-92e1-ad26a8c60279';    
+    var MAC_Value = crypto.createHash('sha256').update(JSON.stringify(txnReq) + secretKey).digest('base64');
+
+    res.render('NETSpayment', { txnReq: JSON.stringify(txnReq), hmac: MAC_Value, keyID: keyID });
+})
+
 
 module.exports = router;
